f0970dc646ea17da7ffa386508137cdbb052ff28
islamrokon <54747900+islamrokon@users.noreply.github.com>
2023-02-28 11:28:31 +0600

    fixed

--- src/Admin/adminmodule.module.ts
+++ src/Admin/adminmodule.module.ts
@@ -2,7 +2,7 @@ import { Controller, Module } from "@nestjs/common";
 import { AdminController } from "./admin.controller";
 import { adminservice } from "./adminservice.service";
 import { TypeOrmModule } from "@nestjs/typeorm";
-import { AdminEntity} from "./adminentity.entity"
+
 
 
 @Module({
